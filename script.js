9817d27f276ac0bb68abc990d5f1592714f11bc1
SaswatTripathi <saswattripathii12@gmail.com>
2019-05-12 18:12:59 +0545

    sub folders were deleted

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
-var email = document.getElementById("email");
-var fnamebox = document.getElementById("fnamebox");
-var lnamebox = document.getElementById("lnamebox");
-var emailbox = document.getElementById("emailbox");
+var fname = document.getElementById("fname");//getting element having id fname
+var lname = document.getElementById("lname");//getting element having id lname
+var email = document.getElementById("email");//getting element having id email
+var fnamebox = document.getElementById("fnamebox");//getting element having id fnamebox
+var lnamebox = document.getElementById("lnamebox");//getting element having id lnamebox
+var emailbox = document.getElementById("emailbox");//getting element having id emailbox
 
 fname.onblur = function(){
 	if(fname.value==""){
-		fnamebox.innerHTML = "Fill the Box";
+		fnamebox.innerHTML = "This field is empty";//if the field in id fname is clicked and left empty this message is shown by activating id fnamebox
 	}else{
-		fnamebox.innerHTML = "";
+		fnamebox.innerHTML = "";//else nothing is shown
 	}
 }
 
 lname.onblur = function(){
 	if(lname.value==""){
-		lnamebox.innerText = "Fill the Box";
+		lnamebox.innerText = "This field is empty";//if the field in id lname is clicked and left empty this message is shown by activating id lnamebox
 	}else{
-		lnamebox.innerText = "";
+		lnamebox.innerText = "";//else nothing is shown
 	}
 }
 
 email.onblur = function(){
 	if(email.value==""){
-		emailbox.innerHTML = "Fill the Box";
+		emailbox.innerHTML = "This field is empty";//if the field in id email is clicked and left empty this message is shown by activating id emailbox
 	}else{
-		emailbox.innerHTML = "";
+		emailbox.innerHTML = "";//else nothing is shown
 	}
 }
